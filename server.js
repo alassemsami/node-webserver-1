9aa638b317761986805e71f58ddad78aa7b486b2
Sami Alassem <samialassem@gmail.com>
2018-12-11 03:22:55 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+const port = 3000;
+
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');	
+
+hbs.registerHelper('currentYear',() =>{
+	return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`
+
+	fs.appendFile('server.log', log + '\n', (err) => {
+			if (err){
+				console.log("Something is wrong");
+			}});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('main.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req,res) => {
+	res.render("home.hbs",{
+		title : "Home Page",
+		'name': 'Sami Alassem',
+		'hobbies' : {
+			'work':[
+				'Technology', 'AI', 'Consulting', 'Sales'],
+			'life':[
+				'Football', 'Swimming']},
+	});
+});
+
+app.get('/about', (req,res) => {
+	res.render("about.hbs", {
+		title: 'About Page'
+	});
+});
+
+app.get("/bad",(req,res) => {
+	res.send({
+		'errorMessage': 'Unable to Handle Request.'
+	});
+});
+
+app.listen(port, () =>{
+	console.log(`Server in up on port ${3000}`);
+})
+
